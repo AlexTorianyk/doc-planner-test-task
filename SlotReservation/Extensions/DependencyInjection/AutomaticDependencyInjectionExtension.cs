963396a8d66f71177cd35c6a58a1542173f6770c
Alex Toryanik <alex1toryanik@gmail.com>
2024-06-03 09:05:51 +0200

    feature: automatic DI

---
+++ SlotReservation/Extensions/DependencyInjection/AutomaticDependencyInjectionExtension.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class AutomaticDependencyInjectionExtension
+    {
+        public static void AddDependencies(this IServiceCollection services)
+        {
+            var assemblies = (from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                from referencedAssembly in assembly.GetReferencedAssemblies()
+                where referencedAssembly.Name != null && referencedAssembly.Name.StartsWith("google")
+                select Assembly.Load(referencedAssembly)).ToList();
+
+            assemblies.Add(Assembly.GetCallingAssembly());
+
+            services.Scan(scan => scan
+                .FromAssemblies(assemblies)
+                .AddClasses(classes => classes.AssignableTo<ITransient>())
+                .AsSelfWithInterfaces()
+                .WithTransientLifetime()
+                .AddClasses(classes => classes.AssignableTo<IScoped>())
+                .AsSelfWithInterfaces()
+                .WithScopedLifetime()
+                .AddClasses(classes => classes.AssignableTo<ISingleton>())
+                .AsSelfWithInterfaces()
+                .WithSingletonLifetime());
+        }
+    }
