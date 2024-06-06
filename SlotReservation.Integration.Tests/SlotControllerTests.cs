6c56ff07840a75b574546c1f702ef85a3f33fdd0
Alex Toryanik <alex1toryanik@gmail.com>
2024-06-06 12:29:58 +0200

    feature: SlotController POST method e2e test

--- SlotReservation.Integration.Tests/SlotControllerTests.cs
+++ SlotReservation.Integration.Tests/SlotControllerTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
+using System.Text;
+using System.Text.Json;
 
 namespace SlotReservation.Integration.Tests
 {
@@ -50,6 +52,58 @@ namespace SlotReservation.Integration.Tests
                 // Assert
                 Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
             }
+        }
+
+    public class ReserveSlotTests : SlotControllerTests
+    {
+        public ReserveSlotTests(WebApplicationFactory<Program> factory) : base(factory)
+        {
+        }
+
+        // We'd need a cancellation endpoint to run this test multiple times, otherwise the slot will be busy
+        // [Fact]
+        // public async Task WhenReserveSlotWithValidRequest_ShouldReturnOk()
+        // {
+        //     // Arrange
+        //     var client = _factory.CreateClient();
+        //     var reservationRequest = new ReservationRequest
+        //     {
+        //         FacilityId = "38df4bee-26d9-4feb-a49c-cc49e078982c", // Set the FacilityId property
+        //         Patient = new Patient(),
+        //         Start = new DateTime(2024, 06, 03, 10, 0, 0),
+        //         End = new DateTime(2024, 06, 03, 10, 10, 0)
+        //     };
+        //     var json = JsonSerializer.Serialize(reservationRequest);
+        //     var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        //     // Act
+        //     var response = await client.PostAsync("/slot/reserve", content);
+
+        //     // Assert
+        //     Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        // }
+
+        [Fact]
+        public async Task WhenReserveSlotWithInvalidRequest_ShouldReturnBadRequest()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            var reservationRequest = new ReservationRequest
+            {
+                FacilityId = "123", // Set the FacilityId property
+                Start = new DateTime(2024, 06, 03, 10, 0, 0),
+                End = new DateTime(2024, 06, 03, 9, 0, 0), // End time is before start time, which is invalid,
+                Patient = new Patient()
+            };
+            var json = JsonSerializer.Serialize(reservationRequest);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            // Act
+            var response = await client.PostAsync("/slot/reserve", content);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
         }
     }
 }
